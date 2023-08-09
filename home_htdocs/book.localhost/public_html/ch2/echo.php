7609a865c4cbdbd870af05435fa8a7fed4f4c0c6
최형규 <aakkaa13579@gmail.com>
2023-08-09 16:02:30 +0900

    0809

---
+++ home_htdocs/book.localhost/public_html/ch2/echo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $filename = "cat.jpg";
+    $filename1 = "rabit.jpg";
+    echo "<img src = '$filename'>";
+    echo "<br>";
+    echo "<img src = '$filename1'>";
+    echo "<br>";
+    ?>
+</body>
+</html>
